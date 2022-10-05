e38d61977e28bb095d35d4adbac9d134ea3dfd20
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-05 15:43:52 +0200

    Added mouse offset and comments

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+// Drag related elements
 const dragBox = document.querySelector('.drag-me');
 const dragText = dragBox.querySelector('.box-text');
 
+// Drop box related elements
 const dropBox = document.querySelector('.drop-box');
 const dropText = dropBox.querySelector('.box-text');
 const boundingRect = dropBox.getBoundingClientRect();
 
+// Reset btn
 const resetBtn = document.querySelector('.reset');
 
+// Event listener used for adding opacity when element is dragged
 dragBox.addEventListener('dragstart', () => {
     dragBox.classList.add('dragged');
 });
 
+// Event listener used for adding dragged element into empty space and allowing the reset btn to appear
 dragBox.addEventListener('dragend', event => {
     dragBox.classList.remove('dragged');
-    const [mouseX, mouseY] = [event.clientX, event.clientY];
+    const [mouseX, mouseY] = [event.clientX - 10, event.clientY - 10];
 
     if(mouseX >= boundingRect.left && mouseX <= boundingRect.right && mouseY >= boundingRect.top && mouseY <= boundingRect.bottom)
     {
@@ -25,6 +30,7 @@ dragBox.addEventListener('dragend', event => {
     }
 });
 
+// Event listener used to reset the drag and drop boxes
 resetBtn.addEventListener('click', () => {
     dropText.innerHTML = 'Drag';
     dropBox.classList.add('drop-box');
