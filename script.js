64d276fcd25a394655560bf6d59c2309136df89e
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-05 15:39:58 +0200

    Added drag and reset btn functionality

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log(1);
+const dragBox = document.querySelector('.drag-me');
+const dragText = dragBox.querySelector('.box-text');
+
+const dropBox = document.querySelector('.drop-box');
+const dropText = dropBox.querySelector('.box-text');
+const boundingRect = dropBox.getBoundingClientRect();
+
+const resetBtn = document.querySelector('.reset');
+
+dragBox.addEventListener('dragstart', () => {
+    dragBox.classList.add('dragged');
+});
+
+dragBox.addEventListener('dragend', event => {
+    dragBox.classList.remove('dragged');
+    const [mouseX, mouseY] = [event.clientX, event.clientY];
+
+    if(mouseX >= boundingRect.left && mouseX <= boundingRect.right && mouseY >= boundingRect.top && mouseY <= boundingRect.bottom)
+    {
+        dropText.innerHTML = 'Content';
+        dropBox.classList.remove('drop-box');
+        dropBox.classList.add('drag-me');
+        dragBox.style.visibility = 'hidden';
+        resetBtn.style.display = 'inline-block';
+    }
+});
+
+resetBtn.addEventListener('click', () => {
+    dropText.innerHTML = 'Drag';
+    dropBox.classList.add('drop-box');
+    dropBox.classList.remove('drag-me');
+    dragBox.style.visibility = 'visible';
+    resetBtn.style.display = 'none';
+});
